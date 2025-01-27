54e39edd31402c1dfa0df137e192664e4c35d9e8
andrei-laptop <andrei.massaini@hotmail.com>
2025-01-27 05:21:40 -0300

    adding iterative impl aswell

--- cpp/binary_search/binarySearch.cpp
+++ cpp/binary_search/binarySearch.cpp
@@ -21,13 +21,30 @@ int bSearchRec(vector<int>& nums, int target, int left, int right) {
 int search(vector<int>& nums, int target) {
     return bSearchRec(nums, target, 0, nums.size());
 }
+
+int bSearchIterative(vector<int> nums, int target) {
+    int l = 0;
+    int r = nums.size();
+    while (l < r) {
+        int m = (l + r) / 2;
+        if (nums[m] == target)
+            return m;
+        else if (nums[m] > target) {
+            r = m - 1;
+            continue;
+        } else {
+            l = m + 1;
+            continue;
+        }
+    }
+    return -1;
+}
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
     vector<int> input = {-1, 0, 2, 4, 6, 8};
     int target = 3;
-
-    cout << search(input, target) << endl;
+    // cout << search(input, target) << endl;
+    cout << bSearchIterative(input, target) << endl;
     return 0;
 }
