0408a5a4faa53b31c2ba44abd19317bd9a9c9ae9
andrei-laptop <andrei.massaini@hotmail.com>
2025-03-10 00:29:45 -0300

    3sumimprov

---
+++ cpp/stack/dailyTemp.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define endl '\n'
+
+vector<int> dailyTemperatures(vector<int>& temperatures) {
+    stack<pair<int, int>> st;
+
+    vector<int> ans;
+    return ans;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    return 0;
+}
--- cpp/two_pointers/3sum.cpp
+++ cpp/two_pointers/3sum.cpp
@@ -5,41 +5,35 @@ using namespace std;
 #define pb push_back
 #define all(x) x.begin(), x.end()
 #define endl '\n'
+
 vector<vector<int>> threeSum(vector<int>& nums) {
-    sort(nums.begin(), nums.end());
     vector<vector<int>> ans;
-    int n = nums.size();
-    for (int i = 0; i < n - 2; i++) {
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < nums.size(); i++) {
+        int left = i + 1;
+        int right = nums.size() - 1;
         if (i > 0 && nums[i] == nums[i - 1]) continue;
-        int k = i + 1;
-        int j = n - 1;
-        while (k < j) {
-            int sum = nums[i] + nums[k] + nums[j];
-            if (sum > 0) j--;
-            else if (sum < 0) k++;
+        while (left < right) {
+            int sum = nums[i] + nums[left] + nums[right];
+            if (sum > 0)
+                right--;
+            else if (sum < 0)
+                left++;
             else {
-                ans.push_back({ nums[i], nums[k], nums[j] });
-                k++;
-                j--;
-                while (k < j && nums[k] == nums[k - 1]) k++;
-                while (k < j && nums[j] == nums[j + 1]) j--;
+                ans.push_back({nums[i], nums[left], nums[right]});
+                while (left < right && nums[left] == nums[left + 1]) left++;
+                while (right > left && nums[right - 1] == nums[right]) right--;
+                left++;
+                right--;
             }
         }
     }
     return ans;
 }
+
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
-    vector<int> input = { -1, 0, 1, 2, -1, -4 };
-    vector<vector<int>> output = threeSum(input);
-    for (const auto& vec : output) {
-        for (int val : vec) {
-            cout << val << " ";
-        }
-        cout << endl;
-    }
 
     return 0;
 }
