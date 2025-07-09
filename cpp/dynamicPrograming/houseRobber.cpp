3d1af6f7ae926c62b78482755f03f577764c3ec2
Andreixzc <andrei.massaini@hotmail.com>
2025-07-09 05:55:43 -0300

    adding bottom-up approach aswell (house robber

--- cpp/dynamicPrograming/houseRobber.cpp
+++ cpp/dynamicPrograming/houseRobber.cpp
@@ -14,9 +14,18 @@ int dp(vector<int>& nums, vector<int>& memo, int i) {
     return memo[i] = max(nums[i] + dp(nums, memo, i + 2), dp(nums, memo, i + 1));
 }
 
-int rob(vector<int>& nums) {
+int robRecursive(vector<int>& nums) {
     vector<int> memo(nums.size(), -1);
     return dp(nums, memo, 0);
+}
+
+int rob(vector<int>& nums) {
+    vector<int> dp(nums.size(), 0);
+    dp[0] = nums[0];
+    dp[1] = max[nums[0], nums[1]];
+    for (int i = 2; i < nums.size(); i++) dp[i] = max(dp[i - 1], dp[i - 2] + nums[i]);
+
+    return dp[nums.size() - 1];
 }
 int main() {
 
