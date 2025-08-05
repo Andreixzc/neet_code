7efb04404012a2c682cd8b445535ef5a1b3e00e9
Andreixzc <andrei.massaini@hotmail.com>
2025-08-05 00:01:22 -0300

    getting decodeways pattern

--- cpp/dynamicPrograming/decodeWays.cpp
+++ cpp/dynamicPrograming/decodeWays.cpp
@@ -5,49 +5,38 @@ using namespace std;
 #define all(x) x.begin(), x.end()
 #define endl '\n'
 
-int dp(string s, string partition, int start, int end, unordered_set<string>& memo) {
-    if (start >= s.length()) {
-        cout << partition.substr(0, partition.length() - 1) << endl;
-        return 1;
+int dp(string s, int start, unordered_map<int, int>& memo) {
+
+    if (start >= s.length()) return 1;
+    if (memo.find(start) != memo.end()) return memo[start];
+
+    int branchResult = 0;
+
+    if (s[start] != '0') {
+        int val = s[start] - '0';
+        if (val >= 1 && val <= 26) branchResult += dp(s, start + 1, memo);
     }
 
-    if (memo.find(partition) != memo.end()) {
-        return 0;
-    }
-    memo.insert(partition);
-
-    int result = 0;
-
-    string oneChar = s.substr(start, 1);
-    if (oneChar[0] != '0') {
-        int val = stoi(oneChar);
-        if (val >= 1 && val <= 26) {
-            result += dp(s, partition + oneChar + ",", start + 1, end, memo);
+    if (start < s.length() - 1) {
+        if (s[start] != '0') {
+            int val = stoi(s.substr(start, 2));
+            if (val >= 1 && val <= 26) branchResult += dp(s, start + 2, memo);
         }
     }
 
-    if (start + 1 < s.length()) {
-        string twoChar = s.substr(start, 2);
-        if (twoChar[0] != '0') {
-            int val = stoi(twoChar);
-            if (val >= 1 && val <= 26) {
-                result += dp(s, partition + twoChar + ",", start + 2, end, memo);
-            }
-        }
-    }
-
-    return result;
+    return memo[start] = branchResult;
 }
 
 int numDecodings(string s) {
-    unordered_set<string> memo;
-    return dp(s, "", 0, 0, memo);
+    unordered_map<int, int> memo;
+    return dp(s, 0, memo);
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    string s = "06";
-    cout << numDecodings(s) << endl;
+    string str = "1206";
+
+    cout << numDecodings(str) << endl;
     return 0;
 }
