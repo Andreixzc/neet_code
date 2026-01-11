66831664c2e8d29e048433f53a96edadb775a857
Andreixzc <andrei.massaini@hotmail.com>
2026-01-11 18:36:14 -0300

    fixing edge case

--- cpp/two_pointer/is_palindrome.cpp
+++ cpp/two_pointer/is_palindrome.cpp
@@ -10,8 +10,8 @@ bool isPalindrome(string s) {
     int left = 0;
     int right = s.length() - 1;
     while (left < right) {
-        while (left < right && !isalpha(s[left])) left++;
-        while (left < right && !isalpha(s[right])) right--;
+        while (left < right && !isalnum(s[left])) left++;
+        while (left < right && !isalnum(s[right])) right--;
         if (tolower(s[left]) != tolower(s[right])) return false;
         left++;
         right--;
