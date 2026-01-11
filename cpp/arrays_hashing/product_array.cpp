f4e87d536fe8899157a79a85d94772d9ab6a9076
Andreixzc <andrei.massaini@hotmail.com>
2026-01-11 01:16:15 -0300

    fix garbage code

--- cpp/arrays_hashing/product_array.cpp
+++ cpp/arrays_hashing/product_array.cpp
@@ -5,27 +5,17 @@ using namespace std;
 #define pb push_back
 #define all(x) x.begin(), x.end()
 #define endl '\n'
+
 vector<int> productExceptSelf(vector<int>& nums) {
     int n = nums.size();
-    vector<int> prefix(n), postfix(n), ans(n);
-
-    if (n == 0) return ans;
-
+    vector<int> ans(n);
+    vector<int> prefix(n);
+    vector<int> sufix(n);
     prefix[0] = 1;
-    postfix[n - 1] = 1;
-
-    for (int i = 1; i < n; i++) {
-        prefix[i] = prefix[i - 1] * nums[i - 1];
-    }
-
-    for (int i = n - 2; i >= 0; i--) {
-        postfix[i] = postfix[i + 1] * nums[i + 1];
-    }
-
-    for (int i = 0; i < n; i++) {
-        ans[i] = prefix[i] * postfix[i];
-    }
-
+    sufix[n - 1] = 1;
+    for (int i = 1; i < nums.size(); i++) prefix[i] = nums[i - 1] * prefix[i - 1];
+    for (int i = n - 2; i >= 0; i--) sufix[i] = nums[i + 1] * sufix[i + 1];
+    for (int i = 0; i < nums.size(); i++) ans[i] = sufix[i] * prefix[i];
     return ans;
 }
 
@@ -34,10 +24,9 @@ int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     vector<int> input = { 1, 2, 4, 6 };
-    input.pb(1);
-    vector<int> output;
-    output = productExceptSelf(input);
-    for (int e : output) cout << e << endl;
+    vector<int> ans = productExceptSelf(input);
+
+    for (int i : ans) cout << i << " ";
 
     return 0;
 }
