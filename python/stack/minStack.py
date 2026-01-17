087efb1afaea8e0281a4b1fc399d5cee20efdec5
Andreixzc <andrei.massaini@hotmail.com>
2026-01-17 20:57:33 -0300

    min stack py

---
+++ python/stack/minStack.py
@@ -0,0 +1,35 @@
+import sys
+import threading
+from typing import List, Tuple, Dict, Set
+from collections import defaultdict, deque
+
+# from heapq import heappush, heappop  # For priority queues
+
+
+class MinStack:
+
+    def __init__(self):
+        self.st = []
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.stack.append((val, val))
+        else:
+            self.stack.append((val, min(val, self.stack[-1][1])))
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    threading.Thread(target=main).start()
