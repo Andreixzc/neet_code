8a4a4f409fb7078180113042ea6652d30fecc14a
Andreixzc <andrei.massaini@hotmail.com>
2026-01-31 21:28:05 -0300

    fix shits

--- cpp/linkedList/mergeLinkedList.cpp
+++ cpp/linkedList/mergeLinkedList.cpp
@@ -19,25 +19,39 @@ ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
     if (!list1) return list2;
     if (!list2) return list1;
 
-    ListNode* head = (list1->val < list2->val) ? list1 : list2;
-    ListNode* tail = head;
-    if (head == list1)
-        list1 = list1->next;
-    else
-        list2 = list2->next;
+    ListNode* head;
+    ListNode* sorted;
 
-    while (list1 != nullptr && list2 != nullptr) {
-        if (list1->val < list2->val) {
-            tail->next = list1;
-            list1 = list1->next;
-        } else {
-            tail->next = list2;
-            list2 = list2->next;
-        }
-        tail = tail->next;
+    if (list1->val < list2->val) {
+        head = list1;
+        list1 = list1->next;
+    } else {
+        head = list2;
+        list2 = list2->next;
     }
 
-    tail->next = (list1 != nullptr) ? list1 : list2;
+    sorted = head;
+
+    while (list1 != nullptr && list2 != nullptr) {
+
+        if (list1->val < list2->val) {
+            sorted->next = list1;
+            list1 = list1->next;
+        }
+
+        else {
+            sorted->next = list2;
+            list2 = list2->next;
+        }
+
+        sorted = sorted->next;
+    }
+
+    if (list1 != nullptr) {
+        sorted->next = list1;
+    } else {
+        sorted->next = list2;
+    }
 
     return head;
 }
