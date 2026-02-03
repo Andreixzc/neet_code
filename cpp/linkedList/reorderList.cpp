de9926017d200e84173afccbcf8b12048312e2f5
Andreixzc <andrei.massaini@hotmail.com>
2026-02-03 20:09:03 -0300

    reorder list garbage impl

--- cpp/linkedList/reorderList.cpp
+++ cpp/linkedList/reorderList.cpp
@@ -19,38 +19,36 @@ void reorderList(ListNode* head) {
     if (!head || !head->next) return;
 
     int count = 0;
-    ListNode* temp = head;
-    while (temp != nullptr) {
+    ListNode* tmpHead = head;
+    while (tmpHead != nullptr) {
         count++;
-        temp = temp->next;
+        tmpHead = tmpHead->next;
     }
 
-    ListNode* mid = head;
-    for (int i = 0; i < (count - 1) / 2; i++) {
-        mid = mid->next;
-    }
+    int mid = (count - 1) / 2;
 
-    ListNode* second = mid->next;
-    mid->next = nullptr;
+    ListNode* midNode = head;
+    for (int i = 0; i < mid; i++) midNode = midNode->next;
+
+    ListNode* secondHalf = midNode->next;
+    midNode->next = nullptr;
 
     ListNode* prev = nullptr;
-    ListNode* current = second;
-    while (current != nullptr) {
-        ListNode* tmpNext = current->next;
-        current->next = prev;
-        prev = current;
-        current = tmpNext;
+    ListNode* curr = secondHalf;
+    while (curr != nullptr) {
+        ListNode* next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
     }
 
     ListNode* first = head;
-    second = prev;
+    ListNode* second = prev;
     while (second != nullptr) {
         ListNode* tmp1 = first->next;
         ListNode* tmp2 = second->next;
-
         first->next = second;
         second->next = tmp1;
-
         first = tmp1;
         second = tmp2;
     }
@@ -68,14 +66,10 @@ void printList(ListNode* head) {
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    // Method 1: Manual creation
-    ListNode* head1 = new ListNode(1);
-    head1->next = new ListNode(2);
-    head1->next->next = new ListNode(3);
-    head1->next->next->next = new ListNode(4);
 
-    cout << "List 1:" << endl;
-    printList(head1);
+    int n = 4;
+    vector<int> arr = { 1, 2, 3, 4, 5 };
+    cout << arr[n / 2];
 
     return 0;
 }
