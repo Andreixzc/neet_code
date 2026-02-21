61d03e737e215165ac505a6130a68190012cb35e
Andreixzc <andrei.massaini@hotmail.com>
2026-02-21 02:20:36 -0300

    myown shit

--- cpp/linkedList/add_two_numbers.cpp
+++ cpp/linkedList/add_two_numbers.cpp
@@ -25,12 +25,13 @@ struct ListNode {
 
 ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
 
-    vector<int> rawSum;
     int carry = 0;
+    vector<int> rawSum;
 
     while (l1 != nullptr || l2 != nullptr) {
         int cl1 = 0;
         int cl2 = 0;
+
         if (l1 != nullptr) {
             cl1 = l1->val;
             l1 = l1->next;
@@ -40,20 +41,21 @@ ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
             cl2 = l2->val;
             l2 = l2->next;
         }
-
         int sum = cl1 + cl2 + carry;
         carry = sum / 10;
         rawSum.push_back(sum % 10);
     }
     if (carry > 0) rawSum.push_back(carry);
-    ListNode* dummy = new ListNode(0);
-    ListNode* curr = dummy;
-    for (int digit : rawSum) {
-        curr->next = new ListNode(digit);
-        curr = curr->next;
+
+    ListNode* head = new ListNode(0);
+    ListNode* it = head;
+
+    for (int element : rawSum) {
+        it->next = new ListNode(element);
+        it = it->next;
     }
 
-    return dummy->next;
+    return head->next;
 }
 
 int main() {
@@ -61,14 +63,13 @@ int main() {
     cin.tie(0);
 
     // Create first linked list: 2 -> 4 -> 3
-    ListNode* l1 = new ListNode(1);
-    l1->next = new ListNode(2);
-    l1->next->next = new ListNode(3);
+    ListNode* l1 = new ListNode(9);
+    l1->next = new ListNode(7);
+    l1->next->next = new ListNode(5);
 
     // Create second linked list: 5 -> 6 -> 4
-    ListNode* l2 = new ListNode(4);
-    l2->next = new ListNode(5);
-    l2->next->next = new ListNode(6);
+    ListNode* l2 = new ListNode(8);
+    l2->next = new ListNode(6);
     addTwoNumbers(l1, l2);
 
     return 0;
